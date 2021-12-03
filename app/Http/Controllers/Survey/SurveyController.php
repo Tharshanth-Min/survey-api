3202ea0fcee833d5a372e77c275bacd6e714628e
Tharshanth-Min <tharsanthsanth4@gmail.com>
2021-12-03 09:09:39 +0530

    new changes

--- app/Http/Controllers/Survey/SurveyController.php
+++ app/Http/Controllers/Survey/SurveyController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\ResponseCollection;
 use App\Models\Survey;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 
 class SurveyController extends Controller {
@@ -13,12 +14,19 @@ class SurveyController extends Controller {
     public function getAll(Request $request) {
 
         try {
-            if ($request->has('search_query')) {
-                $q = $request->input('search_query', 'null');
-                $surveys = Survey::where('name', 'LIKE', '%'.$q.'%')
-                    ->paginate($request->per_page);
-                return new ResponseCollection($surveys);
-            }
+            $files = Storage::allFiles("surveys");
+
+            return response()->json([
+                'status' => 200,
+                'surveys' => count($files)
+            ], 200);
+
+//            if ($request->has('search_query')) {
+//                $q = $request->input('search_query', 'null');
+//                $surveys = Survey::where('name', 'LIKE', '%'.$q.'%')
+//                    ->paginate($request->per_page);
+//                return new ResponseCollection($surveys);
+//            }
         }catch(\Exception $e) {
             return response()->json([
                 'status' => 500,
